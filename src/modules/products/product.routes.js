9058088e50de4ca974e0e33ae63fc9a5366fd9a3
Al Chyrill Cabrera <alc@payrollpie.com>
2017-12-28 14:16:45 +0800

    added crud for Product, added additional helpers

--- src/modules/products/product.routes.js
+++ src/modules/products/product.routes.js
@@ -4,6 +4,11 @@ import * as ProductController from './product.controller';
 
 const routes = new Router();
 
-routes.post('/create',ProductController.create);
+routes.post('',ProductController.create);
+routes.delete('/:id',ProductController.remove);
+routes.put('',ProductController.update);
+routes.get('/:id',ProductController.getById);
+routes.get('',ProductController.search);
+routes.get('/all',ProductController.getAll);
 
 export default routes;
